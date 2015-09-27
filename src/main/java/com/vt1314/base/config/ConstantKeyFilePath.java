bd54b96a3a25b3841ee9a05b40ea45097133d0c0
val.jzp <valjzp@hotmail.com>
2015-09-27 12:30:28 +0800

    Refactor Code.

---
+++ src/main/java/com/vt1314/base/config/ConstantKeyFilePath.java
@@ -0,0 +1,19 @@
+package com.vt1314.base.config;
+
+/**
+ * Author: 居泽平  Date: 13-11-23, 下午7:43
+ */
+public class ConstantKeyFilePath {
+
+	/**
+	 * 文件保存的根目录
+	 */
+	public static final String FILE_UPLOAD_ROOT_KEY = "fileSaveRootPath";
+
+	/**
+	 * 文件上传路径配置
+	 *
+	 * public static final String EXAMPLE_DIR = "exampleUploads/example";
+	 * public static final String EXAMPLE_FIX = "e";
+	 */
+}
--- src/main/java/com/vt1314/config/ConstantKeySession.java
+++
@@ -1,9 +0,0 @@
-package com.vt1314.config;
-
-/**
- * Author: 陈梨春  Date: 14-11-5
- */
-public class ConstantKeySession {
-
-	public static final String WEB_USER_INFO_KEY = "loginAgent";
-}
